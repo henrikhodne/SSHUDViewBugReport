3bd2bb34bdb2637090748fca5b37da8087b74bb9
Henrik Hodne <me@henrikhodne.com>
2012-11-07 23:00:33 -0600

    Initial Commit

---
+++ SSHUDViewBug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SSHUDViewBug
+//
+//  Created by Henrik Hodne on 11/7/12.
+//  Copyright (c) 2012 Henrik Hodne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSHUDViewBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSHUDViewBug
+//
+//  Created by Henrik Hodne on 11/7/12.
+//  Copyright (c) 2012 Henrik Hodne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
